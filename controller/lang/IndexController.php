b3450f2387e34c926cd147169c7903742099c0bb
Jie <cnzhujie@qq.com>
2020-05-08 23:38:09 +0800

    语言包修改

--- controller/lang/IndexController.php
+++ controller/lang/IndexController.php
@@ -2,13 +2,24 @@
 // +-------------------------------------------------------------------------------
 // | DiLu [ A Rapid Development Framework For Enterprise ]
 // +-------------------------------------------------------------------------------
-// | en-us.php v1.0
+// | IndexController.php v1.0
 // |
 // | Copyright (c) 2020 https://dilu.dev All rights reserved.
 // | Author : Jie
-// | Date : 2020/5/2
+// | Date : 2020/5/8
 // +-------------------------------------------------------------------------------
 
-return [
-    'SUCCESS' => 'operate success'
-];
+namespace app\demo\controller\lang;
+
+
+use app\core\controller\common\BaseController;
+
+class IndexController extends BaseController {
+    protected function checkLogin() {
+    }
+
+    public function index() {
+        $this->assign("langFromPhp", lang("SUCCESS"));
+        return view();
+    }
+}
